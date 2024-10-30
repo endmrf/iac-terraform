e4039c00196d297aef7b3e87e7e06dd573c4354e
Endryo Moreira Rodrigues Feitosa <endryofeitosa@gmail.com>
2024-10-29 21:32:28 -0300

    implement terraform with aws provider

---
+++ main.tf
@@ -0,0 +1,70 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_key_pair" "my-key" {
+    key_name = "my-key"
+    public_key = file("C:/Users/endry/.ssh/id_rsa.pub")
+}
+
+resource "aws_security_group" "my-sg" {
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    self = true
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+  }
+  
+}
+
+resource "aws_instance" "my-instance" {
+    ami = "ami-0866a3c8686eaeeba"
+    instance_type = "t2.micro"
+    key_name = "my-key"
+    # count = 1
+    tags = {
+      Name = "unifor"
+      type = "universidade"
+    }
+    security_groups = [ "${aws_security_group.my-sg.name}" ]
+    provisioner "remote-exec" {
+      connection {
+        type = "ssh"
+        user = "ubuntu"
+        private_key = file("C:/Users/endry/.ssh/id_rsa")
+        host = self.public_ip
+      }
+      inline = [ 
+        "sudo apt update -y",
+        "sudo apt install -y nginx",
+        "git clone https://github.com/fcomateus/iac-example.git",
+        "cd iac-example/src/",
+        "sudo cp index.html script.js /var/www/html/"
+      ]
+    }
+}
+
+output "instance_ip" {
+  value = aws_instance.my-instance.public_ip
+}
